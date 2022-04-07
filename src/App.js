785c55694c8523265256de3ceb33bea19e33d4e9
Debbie Hall <debleehall15@gmail.com>
2022-04-06 21:43:52 -0400

    contact added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/Resume";
+import Contact from "./components/Contact/Contact";
 import {
   BrowserRouter as Router,
   Route,
@@ -25,6 +26,7 @@ function App() {
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
       </div>
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ContactForm from "./ContactForm";
+import { Container, Row, Col } from "react-bootstrap";
+
+function Contact() {
+    return (
+        <Container fluid className="contact-section">
+            <Container>
+                <Row style={{ justifyContent: "center", padding: "10px" }}>
+                    <Col
+                        md={7}
+                        style={{
+                            justifyContent: "center",
+                            paddingTop: "30px",
+                            paddingBottom: "50px",
+                        }}
+                    >
+                        <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+                            Contact <strong className="blue">Me</strong>
+                        </h1>
+                        <ContactForm />
+                    </Col>
+                    <Col
+                        md={5}
+                        style={{ paddingTop: "120px", paddingBottom: "50px" }}
+                    >
+                        <p className="contact-info">Phone: (740) 816-6226</p>
+                        <p className="contact-info">Email: debleehall15@gmail.com</p>
+                    </Col>
+                </Row>
+            </Container>
+        </Container>
+    );
+}
+
+export default Contact;
